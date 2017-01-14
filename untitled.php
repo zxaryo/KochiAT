e49a70e19cbd66d3598ddc39fceebee76b043012
Akhil Chandran <zxaryo@gmail.com>
2017-01-14 11:12:33 +0530

    map

--- untitled.php
+++ untitled.php
@@ -57,6 +57,34 @@
 
     <div id="directions-panel"></div>
     </div>
+    <?php
+
+      $servername = "localhost";
+      $username = "adminvj197IY";
+      $password = "unZ7s7rQx4YqL";
+      $dbname = "kochiat";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      // Check connection
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      } 
+
+      $sql = "SELECT sender, timesamp, value FROM zxdot ORDER BY timesamp DESC";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+              echo  $row["timesamp"]. "| " .$row["sender"].  " : " . $row["value"]. "<br>";
+          }
+      } else {
+          echo "0 results";
+      }
+      $conn->close();
+
+      ?>
     <script>
       function initMap() {
         var directionsService = new google.maps.DirectionsService;
