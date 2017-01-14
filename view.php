ae6267f3c21390c3f77b460947e7c4cf814735d0
Akhil Chandran <zxaryo@gmail.com>
2017-01-14 12:03:17 +0530

    ,

--- view.php
+++ view.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo  $row["sender"].  " : " . $row["value"]. " " .$row["lat"] . " " . $row["lon"] . "<br>";
+        echo  $row["sender"].  " : " . $row["value"]. " " .$row["lat"] . ", " . $row["lon"] . "<br>";
     }
 } else {
     echo "0 results";
