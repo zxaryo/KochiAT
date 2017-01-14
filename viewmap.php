93b4ec2df33491e283403780b155e7cf83dbd813
Akhil Chandran <zxaryo@gmail.com>
2017-01-14 11:03:29 +0530

    lat long

---
+++ viewmap.php
@@ -0,0 +1,28 @@
+<?php
+
+$servername = "localhost";
+$username = "adminvj197IY";
+$password = "unZ7s7rQx4YqL";
+$dbname = "kochiat";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT sender, timesamp, value ,lat, lon FROM zxdot ORDER BY timesamp DESC";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo  $row["timesamp"]. "| " .$row["sender"].  " : " . $row["value"]. "|"  . $row["value"] . "|" . $row["lat"] . "," . $row["lon"]   <br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
