7db81ce70cfa4686f31c1f6bf4fba7fdfc5ab1f9
Akhil Chandran <zxaryo@gmail.com>
2017-01-14 12:28:11 +0530

    kill view

--- view.php
+++ view.php
@@ -13,11 +13,19 @@ if ($conn->connect_error) {
 
 $sql = "SELECT sender,  value, lat ,lon FROM kochiat";
 $result = $conn->query($sql);
-
+$lat=array("");
+$lon=array("");
+$i=1;
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo  $row["sender"].  " : " . $row["value"]. " " .$row["lat"] . ", " . $row["lon"] . "<br>";
+
+            $lat[$i]=.$row["lat"].;
+            $lon[$i]=.$row["lon"].;
+            $i=$i+1;
+            echo $lat[$i];
+            echo $lon[$i];
     }
 } else {
     echo "0 results";
